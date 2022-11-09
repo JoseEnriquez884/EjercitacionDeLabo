bcf835c12807c35aa8c4359b773321830c81b68d
Alumne UTNFRA <alumno@sistemas-utnfra.com>
2022-11-09 12:23:37 -0300

    actualizando

--- lalal/archivos/src/archivos.c
+++ lalal/archivos/src/archivos.c
@@ -4,5 +4,13 @@
 int main(void)
 {
 	setbuf(stdout,NULL);
+	FILE* pFile;
+	pFile=fopen("archivo.txt","w");
+	if(pFile!=NULL)
+	{
+		printf("no se pudo abrir el archivo\n");
+	}
+	fprintf(pFile,"hola mundo");
+	fclose(pFile);
 	return EXIT_SUCCESS;
 }
